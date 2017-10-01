c1d41fed363af7427a8a6e731d5a05b1572a7c89
Kayra <KayraAlat@gmail.com>
2017-10-01 21:20:44 +0100

    Updated config

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
