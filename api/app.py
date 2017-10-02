f2e8ab7d7968a61b89f507f026d340849d33797d
Kayra <KayraAlat@gmail.com>
2017-10-02 21:26:00 +0100

    Updated config

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://metuo:local_insecure_password@localhost:5432/metuo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
