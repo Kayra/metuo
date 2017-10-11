c191af3a60ca6366e18307af54a80e110adb96b4
Kayra <KayraAlat@gmail.com>
2017-10-11 10:54:21 +0100

    Added upload folder config

--- api/app.py
+++ api/app.py
@@ -3,8 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://metuo:local_insecure_password@localhost:5432/metuo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["UPLOAD_FOLDER"] = "./"
+
 db = SQLAlchemy(app)
 
 
