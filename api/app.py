302fd84cf0d8f8f5d342a9acfb2c914ffaaa82cc
Kayra <KayraAlat@gmail.com>
2017-11-02 23:52:00 +0000

    Added helper function for file uploading validation

--- api/app.py
+++ api/app.py
@@ -7,9 +7,21 @@ app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://metuo:local_insecure_password@localhost:5432/metuo"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 app.config["UPLOAD_FOLDER"] = "./"
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 db = SQLAlchemy(app)
+
+
+def allowed_file(file_name):
+
+    try:
+        file_extension = file_name.rsplit(".", 1)[1].lower()
+    except IndexError:
+        return False
+
+    return file_extension in ALLOWED_EXTENSIONS
 
 
 @app.route("/")
@@ -19,6 +31,6 @@ def index():
 
 
 @app.route("/upload")
-def upload():
+def upload_image():
     # Read exif data
     return "Image uploaded"
