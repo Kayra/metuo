6650e1a5358a42f45d94331977f965e0d5e84ac6
Kayra <KayraAlat@gmail.com>
2018-06-12 23:39:00 +0200

    Added click command to init db

--- api/database.py
+++ api/database.py
@@ -1,5 +1,7 @@
-from flask_sqlalchemy import SQLAlchemy
+import click
 from flask import current_app, g
+from flask.cli import with_appcontext
+from flask_sqlalchemy import SQLAlchemy
 
 
 def get_db():
@@ -23,3 +25,12 @@ def init_db():
     db.create_all()
 
 
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """
+        Clear the existing data and create new tables.
+    """
+
+    init_db()
+    click.echo('Successfully initialised the database.')
