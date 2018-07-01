56b6fbcb4d55ba872f37c167422f2e684cbf8e0d
Kayra <KayraAlat@gmail.com>
2018-07-01 20:24:00 +0200

    Refactor

---
+++ api/helpers.py
@@ -0,0 +1,43 @@
+import os
+import io
+
+from flask import current_app as app
+from PIL import Image as PILImage, ExifTags
+from werkzeug.utils import secure_filename
+
+from api.models import Image, db
+
+
+def save_image(image_hex_bytes):
+
+    image = _hex_to_image(image_hex_bytes)
+    exif_data = _format_exif_data(image.getexif())
+
+    image_directory = app.config["IMAGE_DIRECTORY"]
+    image_name = secure_filename('test.jpg')
+    image_location = os.path.join(image_directory, image_name)
+
+    image.save(image_location)
+
+    db_image = Image(name=image_name,
+                     exif_data=exif_data)
+
+    db.session.add(db_image)
+    db.session.commit()
+
+
+def _hex_to_image(image_hex_bytes):
+
+    image_stream = io.BytesIO(image_hex_bytes)
+    image = PILImage.open(image_stream)
+
+    return image
+
+
+def _format_exif_data(unformatted_exif_data):
+
+    return {
+        ExifTags.TAGS[exif_index]: str(exif_data, 'utf-8') if isinstance(exif_data, bytes) else exif_data
+        for exif_index, exif_data in unformatted_exif_data.items()
+        if exif_index in ExifTags.TAGS
+    }
--- api/views.py
+++ api/views.py
@@ -1,15 +1,9 @@
 import os
-import io
 
-from flask import Blueprint, flash, g, redirect, render_template, request, url_for, send_file, current_app as app
-from PIL import Image as PILImage, ExifTags
-from werkzeug.utils import secure_filename
-from werkzeug.exceptions import abort
+from flask import Blueprint, request, send_file, current_app as app
 
-# from flaskr.auth import login_required
-# from api.database import get_db
-
-from api.models import Image, db
+from api.helpers import save_image
+from api.models import Image
 
 
 bp = Blueprint('images', __name__)
@@ -52,36 +46,4 @@ def get_images():
 #     return image_extension in ALLOWED_EXTENSIONS
 
 
-def hex_to_image(image_hex_bytes):
 
-    image_stream = io.BytesIO(image_hex_bytes)
-    image = PILImage.open(image_stream)
-
-    return image
-
-
-def format_exif_data(unformatted_exif_data):
-
-    return {
-        ExifTags.TAGS[exif_index]: str(exif_data, 'utf-8') if isinstance(exif_data, bytes) else exif_data
-        for exif_index, exif_data in unformatted_exif_data.items()
-        if exif_index in ExifTags.TAGS
-    }
-
-
-def save_image(image_hex_bytes):
-
-    image = hex_to_image(image_hex_bytes)
-    exif_data = format_exif_data(image.getexif())
-
-    image_directory = app.config["IMAGE_DIRECTORY"]
-    image_name = secure_filename('test.jpg')
-    image_location = os.path.join(image_directory, image_name)
-
-    image.save(image_location)
-
-    db_image = Image(name=image_name,
-                     exif_data=exif_data)
-
-    db.session.add(db_image)
-    db.session.commit()
