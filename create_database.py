fd26421d76b055ac5c0ade748d7dc8087e89777a
Kayra <KayraAlat@gmail.com>
2018-07-03 20:36:00 +0200

    Clean up

--- create_database.py
+++
@@ -1,7 +0,0 @@
-from api.app import db
-
-try:
-    db.create_all()
-    print("Database created successfully")
-except Exception as exception:
-    print(f"Could not create database due to {exception}")
