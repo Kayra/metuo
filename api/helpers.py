d6a7dc58ebeb3ba6ae60d031cdb25ed3f981d096
Kayra <KayraAlat@gmail.com>
2018-07-09 17:27:00 +0200

    Refactor

--- api/helpers.py
+++ api/helpers.py
@@ -19,28 +19,29 @@ def save_image(image: FileStorage, tags: List[str]):
     image = _hex_to_image(image_hex_bytes)
     exif_data = _format_exif_data(image.getexif())
 
-    image_directory = app.config["IMAGE_DIRECTORY"]
-    image_name = secure_filename(image_name)
-    image_location = os.path.join(image_directory, image_name)
-
-    image.save(image_location)
+    _save_image_locally(image, image_name)
 
     db_image = Image(name=image_name,
                      exif_data=exif_data)
 
     for tag in tags:
-
         tag = Tag.query.filter_by(tag_name=tag).one() if _tag_exists(tag) else Tag(tag_name=tag)
-
         db_image.tags.append(tag)
 
     db.session.add(db_image)
     db.session.commit()
 
 
-def _tag_exists(tag) -> bool:
-    print(Tag.query.filter_by(tag_name=tag).scalar())
+def _save_image_locally(image, image_name) -> None:
 
+    image_directory = app.config["IMAGE_DIRECTORY"]
+    image_name = secure_filename(image_name)
+    image_location = os.path.join(image_directory, image_name)
+
+    image.save(image_location)
+
+
+def _tag_exists(tag) -> bool:
     return bool(Tag.query.filter_by(tag_name=tag).scalar())
 
 
@@ -53,19 +54,8 @@ def _hex_to_image(image_hex_bytes) -> JpegImageFile:
 
 
 def _format_exif_data(unformatted_exif_data):
-
     return {
         ExifTags.TAGS[exif_index]: str(exif_data, 'utf-8') if isinstance(exif_data, bytes) else exif_data
         for exif_index, exif_data in unformatted_exif_data.items()
         if exif_index in ExifTags.TAGS
     }
-
-
-# def allowed_image(image_name):
-#
-#     try:
-#         image_extension = image_name.rsplit(".", 1)[1].lower()
-#     except IndexError:
-#         return False
-#
-#     return image_extension in ALLOWED_EXTENSIONS
