f84f1577ee6ca4cc41788d3cad67634bb48933fe
Kayra <KayraAlat@gmail.com>
2018-08-06 09:34:00 +0200

    Added views integ test structure

---
+++ server/tests/integration/test_views.py
@@ -0,0 +1,13 @@
+from server.views import upload_image, get_images, get_tags
+
+
+class TestViews:
+
+    def test_upload_image(self):
+        pass
+
+    def test_get_images(self):
+        pass
+
+    def test_get_tags(self):
+        pass
