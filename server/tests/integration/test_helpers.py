1843c1ac99deed02dac4cc011488bab2524be911
Kayra <KayraAlat@gmail.com>
2018-08-07 09:34:00 +0200

    Added helpers integ test structure

---
+++ server/tests/integration/test_helpers.py
@@ -0,0 +1,12 @@
+import pytest
+
+from server.helpers import save_image, _save_image_locally
+
+
+class TestHelpers:
+
+    def test_save_image(self):
+        pass
+
+    def test_save_image_locally(self):
+        pass
