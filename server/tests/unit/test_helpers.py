4fb006659a27326d54ce4937723528b4b563836b
Kayra <KayraAlat@gmail.com>
2018-08-17 20:07:00 +0200

    Added inital react app

--- server/tests/unit/test_helpers.py
+++ server/tests/unit/test_helpers.py
@@ -1,14 +1,12 @@
 from unittest import mock
 
 import pytest
+from PIL.JpegImagePlugin import JpegImageFile
 
 from server.helpers import save_image, _hex_to_image, _format_exif_data
 
 
 class TestHelpers:
-
-    def test_save_image(self):
-        pass
 
     def test_hex_to_image(self):
         pass
