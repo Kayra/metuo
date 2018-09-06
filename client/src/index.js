4d229f5230c160c501d0a16d402044bddd473648
Kayra <KayraAlat@gmail.com>
2018-09-06 11:31:00 +0200

    Updated client to request from server

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Image, Filters } from './components';
+import { getTags } from './requests';
 import './styles.css';
 
 export class Page extends React.Component {
@@ -13,10 +14,18 @@ export class Page extends React.Component {
         'Location'
     ];
 
+    // tags = (async () => {
+    //     await getTags();
+    // })();
+
+    tags = getTags().then(tags => tags);
+
     render() { return (
         <div className='page'>
             <div className='image'>
                 <Image src='https://via.placeholder.com/150' alt='' />
+                {/* <p>{this.tags}</p> */}
+                {console.log(this.tags)}
             </div>
             <div className='filters'>
                 <Filters filterCategories={this.filterCategories} /> 
---
+++ client/src/requests.js
@@ -0,0 +1,14 @@
+import * as axios from 'axios';
+
+import { getConfig } from './helpers';
+
+export async function getTags() {
+
+    var config = getConfig();
+    var server = config.server;
+
+    var tags = await axios.get(server + '/tags');
+
+    return tags;
+
+}
