131cb3e32fb3d10d23aab922c934336436c1952e
Dave Reid <davidreiduni@gmail.com>
2019-02-10 21:03:08 +0000

    Hide yo kids, hide yo wife

--- client/src/components/image.js
+++ client/src/components/image.js
@@ -1,91 +1,100 @@
-import React from 'react';
+import React from "react";
+import { Transition, animated } from "react-spring/renderprops";
 
-import { getImages } from '../requests';
-
+import { getImages } from "../requests";
 
 export default class Image extends React.Component {
+  state = {
+    images: [],
+    image: "",
+    index: 0
+  };
 
-    state = {
-        images: [],
-        image: '',
-        index: 0
-    };
+  timeout = undefined;
 
-    loopImages = () => {
+  loopImages = () => {
+    this.timeout = setTimeout(() => {
+      this.nextImage();
+      this.loopImages();
+    }, 3000);
+  };
 
-        setInterval(() => {
+  previousImage = () => {
+    clearTimeout(this.timeout);
+    const previousIndex =
+      this.state.index > 0
+        ? this.state.index - 1
+        : this.state.images.length - 1;
+    this.setState({ index: previousIndex });
+  };
 
-            const index = this.state.index === this.state.images.length - 1 ? 0 : this.state.index + 1;
-            this.setState({index: index})
+  nextImage = () => {
+    clearTimeout(this.timeout);
+    const nextIndex =
+      this.state.index < this.state.images.length - 1
+        ? this.state.index + 1
+        : 0;
 
-        }, 3000);
+    this.setState({ index: nextIndex });
+  };
 
+  keyDown = event => {
+    // eslint-disable-next-line
+    switch (event.keyCode) {
+      case 37:
+        this.previousImage();
+        break;
+      case 39:
+        this.nextImage();
+        break;
     }
+  };
 
-    previousImage = () => {
-        const previousIndex = this.state.index > 0 ? this.state.index - 1 : this.state.images.length - 1;
-        this.setState({index: previousIndex});
-    }
+  async componentDidMount() {
+    const images = await getImages();
+    this.buildImages(images);
+    this.loopImages();
 
-    nextImage = () => {
-        const nextIndex = this.state.index < this.state.images.length - 1 ? this.state.index + 1 : 0;
-        this.setState({index: nextIndex});
-    }
+    document.addEventListener("keydown", this.keyDown.bind(this));
+  }
 
-    keyDown = (event) => {
-        // eslint-disable-next-line
-        switch (event.keyCode) {
-            case 37:
-                this.previousImage();
-                break;
-            case 39:
-                this.nextImage();
-                break;
-        }
-    }
-
-    async componentDidMount() {
-
-        const images = await getImages();
+  async componentDidUpdate(previousProps, previousState) {
+    if (previousProps.tags !== this.props.tags && this.props.tags.length) {
+      const images = await getImages(this.props.tags);
+      if (images.length) {
         this.setState({ images: images });
-        this.loopImages();
-
-        document.addEventListener('keydown', this.keyDown.bind(this));
-
+        this.setState({ index: 0 });
+      }
     }
+  }
 
-    async componentDidUpdate(previousProps, previousState) {
+  buildImages(images) {
+    const imageEls = images.map(img => style => (
+      <animated.img style={style} src={img} key={img} alt={img} />
+    ));
+    this.setState({ images: imageEls });
+  }
 
-        if (previousProps.tags !== this.props.tags && this.props.tags.length) { 
-            const images = await getImages(this.props.tags); 
-            if (images.length) {
-                this.setState({ images: images });
-                this.setState({ index: 0 });
-            }
-        }
+  render() {
+    return (
+      <div className="image">
+        <button onClick={() => this.previousImage()}> &lt; </button>
+        <button onClick={() => this.nextImage()}> &gt; </button>
 
-    }
-
-    render() { 
-
-        const currentImage = this.state.images[this.state.index];
-        const nextImageIndex = this.state.index < this.state.images.length - 1 ? this.state.index + 1 : 0;
-        const nextImage = this.state.images[nextImageIndex]
-
-        return (
-            <div class="image">
-
-                <button onClick={() => this.previousImage()}> &lt; </button>
-
-                <img class="currentImage" src={currentImage} alt=''></img>
-                <img class="nextImage" src={nextImage} alt=''></img>
-
-                <button onClick={() => this.nextImage()}> &gt; </button>
-
-            </div>
-        );
-
-    }
-
+        <div style={{ marginTop: 10 }}>
+          <Transition
+            native
+            reset
+            unique
+            items={this.state.index}
+            from={{ opacity: 0 }}
+            enter={{ opacity: 1 }}
+            leave={{ opacity: 0 }}
+          >
+            {index => this.state.images && this.state.images[index]}
+          </Transition>
+        </div>
+      </div>
+    );
+  }
 }
-
