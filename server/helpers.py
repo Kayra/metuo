36727c7e0469f1a155510e0332a13e00750ab649
Kayra <KayraAlat@gmail.com>
2019-04-08 20:58:00 +0100

    Updated _save_image_to_s3_bucket for prod

--- server/helpers.py
+++ server/helpers.py
@@ -77,9 +77,12 @@ def _save_image_to_s3_bucket(image: FileStorage, image_name: str) -> None:
 
     image_directory = app.config["IMAGE_DIRECTORY"]
 
-    s3_client = boto3.client('s3',
-                             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+    if os.getenv('FLASK_DEBUG') == '0':
+        s3_client = boto3.client('s3')
+    else:
+        s3_client = boto3.client('s3',
+                                 aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
     response = s3_client.upload_fileobj(image, image_directory, image_name)
 
     if response:
