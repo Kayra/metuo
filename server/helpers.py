f561b1d9833814def2e84f058114a5bf74dd7446
Kayra <KayraAlat@gmail.com>
2019-04-22 21:44:00 +0100

    Refactored exif tag extraction

--- server/helpers.py
+++ server/helpers.py
@@ -107,21 +107,21 @@ def _format_exif_data(unformatted_exif_data):
     }
 
 
-def _tags_from_exif(exif_data):
+def _tags_from_exif(exif_data) -> Dict:
 
     full_date = exif_data['DateTime']
     date = full_date.split()[0]
     time = full_date.split()[1]
 
-    year = [date.split(':')[0]]
-    month = [date.split(':')[1]]
-    month_string = [calendar.month_name[month]]
-    day = [date.split(':')[2]]
+    year = date.split(':')[0]
+    month = date.split(':')[1]
+    month_string = calendar.month_name[int(month)]
+    day = date.split(':')[2]
 
     tags = {
-        'Year': year,
-        'Month': month_string,
-        'Day': day
+        'Year': [year],
+        'Month': [month_string],
+        'Day': [day]
     }
 
     return tags
