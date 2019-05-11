3afd1683fb8a8bd90cf67e54b3829a6082e88e97
Kayra <KayraAlat@gmail.com>
2019-05-11 23:17:00 +0100

    Getting API key from env var and added func to determine if current location is in tags

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -6,7 +6,22 @@ export function getConfig() {
     const config = require('./config/' + environment + '.json');
 
     config['env'] = environment;
+    config['ipfindApiKey'] = process.env.REACT_APP_IP_FIND_API_KEY || '';
 
     return config;
+
+}
+
+export function currentLocationInLocationTags(location, categorisedTags) {
+
+    const locationsToCheck = ['country', 'city', 'region', 'county']
+
+    for (const locationToCheck of locationsToCheck) {
+        if (categorisedTags['Location'].includes(location[locationToCheck])) {
+            return location[locationToCheck]
+        }
+    }
+
+    return null;
 
 }
