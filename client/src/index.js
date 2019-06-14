b64a8481d7d785d8a5d0b67f3cd083b9abb4e063
Kayra <KayraAlat@gmail.com>
2019-06-14 17:38:00 +0100

    Moved requests to higher level to set initial values on app load

--- client/src/index.js
+++ client/src/index.js
@@ -5,22 +5,44 @@ import './styles.css';
 import Filters from './components/filters';
 import Image from './components/image';
 
+import { determineToggledCategoryTags } from './helpers';
+import { getCategorisedTags, getLocationInfo } from "./requests";
+
 export class Page extends React.Component {
 
     state = {
         tags: []
     };
 
+    categorisedTags = {};
+    toggledCategoryTags = {};
+
     updateTags = (tags) => {
         this.setState({tags: tags});
+    }
+
+    async componentDidMount() {
+
+        this.categorisedTags = await getCategorisedTags();
+        const location = await getLocationInfo();
+    
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+    
+        this.toggledCategoryTags = determineToggledCategoryTags(location, urlParams, this.categorisedTags);
+    
+        const tags = this.toggledCategoryTags ? Object.keys(this.toggledCategoryTags).map(key => this.toggledCategoryTags[key]) : [];
+
+        this.setState({tags: tags});
+            
     }
 
     render() { 
 
         return (
             <div className='page'>
-                <Image tags={this.state.tags} /> 
-                <Filters updateTags={this.updateTags} /> 
+                <Image tags={this.state.tags}/> 
+                <Filters updateTags={this.updateTags} categorisedTags={this.categorisedTags} toggledCategoryTags={this.toggledCategoryTags} /> 
             </div>
         );
     }
