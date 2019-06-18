ee8e6aaa4e8a414f0e388d2cdd2adeaea097a520
Kayra <KayraAlat@gmail.com>
2019-06-18 22:11:00 +0100

    Added shuffleArray

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -92,3 +92,25 @@ export function urlParamsToCategoryTags(urlParams, categorisedTags) {
 function titleCase(str) {
     return _.startCase(_.toLower(str));
 }
+
+export function shuffleArray(array) {
+
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+
+  }
+
