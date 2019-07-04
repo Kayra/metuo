7d4f8d6e107c6b37eb3a0f5dd5bac2e4b65db421
Kayra <KayraAlat@gmail.com>
2019-07-04 20:29:00 +0100

    Initial _save_image_to_s3_bucket refactor

--- server/image_helpers.py
+++ server/image_helpers.py
@@ -68,3 +68,20 @@ def _delete_image_locally(image_location: str) -> None:
     image_directory = app.config["IMAGE_DIRECTORY"]
     image_location = os.path.join(image_directory, image_location)
     os.remove(image_location)
+
+
+def _save_image_to_s3_bucket(image: FileStorage, image_name: str) -> None:
+
+    image_directory = app.config["IMAGE_DIRECTORY"]
+
+    if is_production():
+        s3_client = boto3.client('s3')
+    else:
+        s3_client = boto3.client('s3',
+                                 aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+    response = s3_client.upload_fileobj(image, image_directory, image_name)
+
+    if response:
+        print(response)
