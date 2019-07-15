040663a1b45161918fa037dfff88980824a6e459
Kayra <KayraAlat@gmail.com>
2019-07-15 20:42:00 +0100

    Initial _delete_image_in_s3_bucket refactor

--- server/helpers/file_helpers.py
+++ server/helpers/file_helpers.py
@@ -30,3 +30,19 @@ def _save_image_file_to_s3_bucket(image: FileStorage, image_name: str) -> None:
     if response:
         print(response)
 
+
+def _delete_image_in_s3_bucket(image_name: str) -> None:
+
+    image_directory = app.config["IMAGE_DIRECTORY"]
+
+    if is_production():
+        s3_client = boto3.client('s3')
+    else:
+        s3_client = boto3.client('s3',
+                                 aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                                 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+    response = s3_client.delete_object(Bucket=image_directory, Key=image_name)
+
+    if response:
+        print(response)
