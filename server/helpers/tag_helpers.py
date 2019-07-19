c198577a6db96f0e8633f3b230d77ab6041ae5c7
Kayra <KayraAlat@gmail.com>
2019-07-19 20:46:00 +0100

    Initial build_categorised_tags refactor

--- server/helpers/tag_helpers.py
+++ server/helpers/tag_helpers.py
@@ -1,5 +1,20 @@
 
 
+def build_categorised_tags(tags: List[Tag]) -> Dict:
+
+    categorised_tags = {}
+
+    for tag in tags:
+
+        category = tag.category.name
+        if category in categorised_tags.keys():
+            categorised_tags[category].append(tag.name)
+        else:
+            categorised_tags[category] = [tag.name]
+
+    return categorised_tags
+
+
 def _tags_from_exif(exif_data: Dict) -> Dict:
 
     full_date = exif_data['DateTimeOriginal']
