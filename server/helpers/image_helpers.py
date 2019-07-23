b4a0e94c1ed0780490bb61f12897a10e171996e8
Kayra <KayraAlat@gmail.com>
2019-07-23 20:58:00 +0100

    Cleaned up image helpers

--- server/helpers/image_helpers.py
+++ server/helpers/image_helpers.py
@@ -1,6 +1,19 @@
-from typing import Dict, List
+import io
+import uuid
+from typing import Dict
 
+from flask import url_for
+from PIL import Image as PILImage, ExifTags
+from PIL.JpegImagePlugin import JpegImageFile
 from werkzeug.datastructures import FileStorage
+
+from server.models import Image, db
+from server.helpers.app_helpers import is_production
+from server.helpers.tag_helpers import update_categorised_tags_with_exif_data
+from server.helpers.file_helpers import save_image_file_locally, \
+                                        delete_image_file_locally, \
+                                        save_image_file_to_s3_bucket, \
+                                        delete_image_file_in_s3_bucket
 
 
 def save_image(uploaded_image: FileStorage, categorised_tags: Dict):
@@ -10,13 +23,13 @@ def save_image(uploaded_image: FileStorage, categorised_tags: Dict):
     uploaded_image.seek(0)
 
     exif_data = _format_exif_data(image.getexif())
-    updated_categorised_tags = _update_tags_with_exif(exif_data, categorised_tags)
+    updated_categorised_tags = update_categorised_tags_with_exif_data(exif_data, categorised_tags)
     image_name = _generate_image_name(uploaded_image.filename, exif_data)
 
     if is_production():
-        _save_image_to_s3_bucket(uploaded_image, image_name)
+        save_image_file_to_s3_bucket(uploaded_image, image_name)
     else:
-        _save_image_locally(image, image_name)
+        save_image_file_locally(image, image_name)
 
     db_image = Image(name=image_name, exif_data=exif_data)
     db_image.add_tags(updated_categorised_tags)
@@ -30,9 +43,9 @@ def remove_image(image_id: str) -> None:
     image = Image.query.filter_by(id=image_id).first()
 
     if is_production():
-        _delete_image_in_s3_bucket(image.name)
+        delete_image_file_in_s3_bucket(image.name)
     else:
-        _delete_image_locally(image.name)
+        delete_image_file_locally(image.name)
 
     db.session.delete(image)
     db.session.commit()
