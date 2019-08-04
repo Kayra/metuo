fe412eef3df6819825705c7d8992c70a46c6e569
Kayra <KayraAlat@gmail.com>
2019-08-04 12:14:00 +0100

    Added test_is_dev

--- server/tests/unit/test_app_helpers.py
+++ server/tests/unit/test_app_helpers.py
@@ -7,19 +7,16 @@ from server.helpers.app_helpers import is_production
 class TestAppHelpers:
 
     def test_is_production(self):
-
         os.environ['PYTHON_ENV'] = 'production'
-
         assert is_production() is True
 
     def test_is_not_production(self):
-
         os.environ['PYTHON_ENV'] = 'definitely_not_production'
-
         assert is_production() is False
 
     def test_is_dev(self):
-        pass
+        os.environ['PYTHON_ENV'] = 'dev'
+        assert is_production() is False
 
     def test_is_production_no_env_set(self):
         pass
