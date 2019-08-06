42df526bd4c23ba9ebb41214fd384bc436d1ab9e
Kayra <KayraAlat@gmail.com>
2019-08-06 12:21:00 +0100

    Added initial image helpers test file

---
+++ server/tests/unit/test_image_helpers.py
@@ -0,0 +1,10 @@
+import os
+
+
+from server.helpers.image_helpers import load_image, _generate_image_name, _hex_to_image, _format_exif_data
+
+
+class TestImageHelpers:
+
+    def test_load_image(self):
+        pass
