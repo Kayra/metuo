75c0827b8ca3127d704f4fd205579de26ea04c1d
Kayra <KayraAlat@gmail.com>
2019-08-07 12:24:00 +0100

    Added initial TestLoadImage class

--- server/tests/unit/test_image_helpers.py
+++ server/tests/unit/test_image_helpers.py
@@ -4,7 +4,10 @@ import os
 from server.helpers.image_helpers import load_image, _generate_image_name, _hex_to_image, _format_exif_data
 
 
-class TestImageHelpers:
+class TestLoadImage:
 
-    def test_load_image(self):
+    def test_load_image_production(self):
+        pass
+
+    def test_load_image_not_production(self):
         pass
