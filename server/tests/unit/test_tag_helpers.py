774984abc8a00a0a47d6d8b7139fca637626e0ae
Kayra <KayraAlat@gmail.com>
2019-08-23 20:00:00 +0100

    Added initial test tag helper classes

---
+++ server/tests/unit/test_tag_helpers.py
@@ -0,0 +1,13 @@
+from server.helpers.tag_helpers import build_categorised_tags, generate_categorised_tags_from_exif_data, update_categorised_tags_with_exif_data
+
+
+class TestBuildCategorisedTags:
+    pass
+
+
+class TestGenerateCategorisedTagsFromExifData:
+    pass
+
+
+class TestUpdateCategorisedTagsWithExifData:
+    pass
