32ad425098570a66b070e826faf3e7c38af952a5
Kayra <KayraAlat@gmail.com>
2019-08-27 21:06:00 +0100

    Added test_update_categorised_tags_with_exif_data

--- server/tests/unit/test_tag_helpers.py
+++ server/tests/unit/test_tag_helpers.py
@@ -5,11 +5,38 @@ from server.helpers.tag_helpers import update_categorised_tags_with_exif_data, \
 class TestUpdateCategorisedTagsWithExifData:
 
     def test_update_categorised_tags_with_exif_data(self, valid_exif_dict, valid_category_tags):
-        pass
+
+        expected_categorised_tags = {
+            "Location": [
+                "Japan"
+            ],
+            "City": [
+                "Tokyo"
+            ],
+            "Colour": [
+                "White"
+            ],
+            "People": [
+                "Rie"
+            ],
+            "Year": [
+                "2020"
+            ],
+            "Month": [
+                "January"
+            ],
+            "Day": [
+                "26"
+            ]
+        }
+
+        actual_categorised_tags = update_categorised_tags_with_exif_data(valid_exif_dict, valid_category_tags)
+        
+        assert expected_categorised_tags == actual_categorised_tags
 
 
 class TestGenerateCategorisedTagsFromExifData:
 
-    def test_generate_categorised_tags_from_exif_data(self):
+    def test_generate_categorised_tags_from_exif_data(self, valid_exif_dict):
         pass
 
